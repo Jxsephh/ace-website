fa32d21d1b41aeab6c01d69d581f91907d9dab06
Matthew <matthewallenturner@gmail.com>
2018-09-23 22:48:45 -0500

    bug fixes

--- flask_app/static_site/routes.py
+++ flask_app/static_site/routes.py
@@ -1,14 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 mod = Blueprint('static', __name__, template_folder='templates', static_folder='static', static_url_path='/static')
 
 @mod.route("/")
 def home():
-    return render_template('index.html')
+    return redirect(url_for('static.index'))
 
 @mod.route('/index')
 def index():
-    return home()
+    return render_template('index.html')
 
 @mod.route('/service')
 def service():
